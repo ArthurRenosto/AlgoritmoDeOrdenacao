115e84f6d9a0837cf319ed503069a620b2c3d632
ArthurRenosto <arthurrenosto123@gmail.com>
2025-05-23 19:41:28 -0300

    aplicando função de numeros aleatorios

--- main.c
+++ main.c
@@ -1,3 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
+void random_generator(){
+    srand(time(NULL));
+    int number = rand() % 1000;
+    printf("%d\n", number);
+}
+
+
+    int main(){
+    random_generator();
+return 0;
+}
